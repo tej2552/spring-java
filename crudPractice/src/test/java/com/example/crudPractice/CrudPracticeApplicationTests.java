16651d0660f338f7b93f597e01964d0b1934da8d
tej2552 <tejkarthik@gmail.com>
2025-02-09 12:26:56 +0530

    created a new spring project

---
+++ crudPractice/src/test/java/com/example/crudPractice/CrudPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
